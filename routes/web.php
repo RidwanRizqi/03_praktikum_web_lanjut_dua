5c586c22c4d7c704b18de29da268f2bab761083e
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-03-01 04:50:56 +0700

    Change Home Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MenuController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProgramController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +19,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/today-special', [HomeController::class, 'todaySpecial']);
-Route::get('/menu', MenuController::class);
+Route::get('/', HomeController::class);
+
+Route::prefix('product')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/product1', [ProductController::class, 'product1']);
+    Route::get('/product2', [ProductController::class, 'product2']);
+    Route::get('/product3', [ProductController::class, 'product3']);
+});
+
+Route::get('/news/{title}', NewsController::class );
+
+Route::prefix('/program')->group(function () {
+    Route::get('/program1', [ProgramController::class, 'program1']);
+    Route::get('/program2', [ProgramController::class, 'program2']);
+    Route::get('/program3', [ProgramController::class, 'program3']);
+});
+
+Route::get('/about-us', AboutUsController::class);
+
+Route::resource('contact-us', ContactUsController::class)->only(['index', 'store']);
