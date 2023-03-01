f9f3922e6b58d3f539a32b3b6a5c6e3fae9e1a76
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-03-01 07:31:19 +0700

    Update View

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,28 +1,28 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container">
-        <a class="navbar-brand" href="index.html"><h2>Stand Blog<em>.</em></h2></a>
+        <a class="navbar-brand" href="{{URL('/')}}"><h2>Stand Blog<em>.</em></h2></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
+                    <a class="nav-link" href="{{URL('/')}}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="about.html">About Us</a>
+                    <a class="nav-link" href="{{URL('/about-us')}}">About Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="blog.html">Product</a>
+                    <a class="nav-link" href="{{URL('/product')}}">Product</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="post-details.html">Program</a>
+                    <a class="nav-link" href="{{URL('/program')}}">Program</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="post-details.html">News</a>
+                    <a class="nav-link" href="{{URL('/news')}}">News</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact Us</a>
+                    <a class="nav-link" href="{{URL('/contact-us')}}">Contact Us</a>
                 </li>
             </ul>
         </div>
